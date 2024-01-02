4124564d1e80b957e89afa0459899eabfa1ec527
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-01-02 16:57:06 -0500

    updated components and App.jsx and index.jsx  and finished lab

--- src/components/pets/PetsList.jsx
+++ src/components/pets/PetsList.jsx
@@ -3,7 +3,7 @@ import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
 
-export const PetsList = ({ pets }) => {
+export const PetsList = ({ pets, type }) => {
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -13,18 +13,25 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+
+  // i feel like i could make a function to set array to a filterd array depending on type instead of having 3 maps in the future
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
-        {cats.map((cat) => (
+        {type === "cat" && cats.map((cat) => (
           <Pet key={cat.id} kind="cat" pet={cat} />
         ))}
 
         {/* All dogs section */}
-        {dogs.map((dog) => (
+        { type === "dog" && dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
+        ))}
+
+        {/* Display other pets if type is not a cat or dog */}
+        { type !== "cat" && type !== "dog" && pets.map((pet) => (
+          <Pet key={pet.id} kind={pet.kind.toLowerCase()} pet={pet} />
         ))}
       </section>
     </section>
