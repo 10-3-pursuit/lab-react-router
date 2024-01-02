aa0c69d8e353a4b086c849c960f60ebb538bb202
Timothy Natal <timothynatal@pursuit.org>
2024-01-02 17:15:04 -0500

    Finished lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import {Route, Routes, useNavigate, useLocation} from "react-router-dom"
 
 /*
   Components
@@ -23,13 +24,33 @@ function App() {
   const [employees] = useState(employeeData);
   const [owners] = useState(ownerData);
   const [pets] = useState(petData);
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  
+  useEffect(() => {
+    if (location.pathname === "/pets" || location.pathname === "/pets/") {
+      navigate("/pets/cats");
+    }
+  }, [location.pathname, navigate]);
+  
   return (
     <div className="wrapper">
       <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home employees={employees} owners={owners} pets={pets} />}
+        />
+        <Route
+          path="/staff"
+          element={<StaffList employees={employees} />}
+        />
+        <Route path="/pets">
+          <Route index element={<PetsList pets={pets}/>}/>
+          <Route path=":kind" element={<PetsList pets={pets}/>}/>
+        </Route>
+      </Routes>
       <Footer />
     </div>
   );
