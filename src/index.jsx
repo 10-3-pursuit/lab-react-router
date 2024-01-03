5a244a5a67eff618b320a3f3a28beeb231dc29f6
juligarc91 <julissagarcia@pursuit.org>
2024-01-02 21:13:28 -0500

    created Root component

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // import BrowserRouter - to track URL addresses
+import { BrowserRouter } from "react-router-dom"; // import BrowserRouter - to track URL addresses and store history (this will give us ability )
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const Root = () => {
+    return (
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
+    );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(Root);
