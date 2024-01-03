e2be43aa36db0edb1a7fb84fd7cc1f8a66494289
juligarc91 <julissagarcia@pursuit.org>
2024-01-03 00:05:18 -0500

    added routes to Pet component

--- src/components/pets/Pet.jsx
+++ src/components/pets/Pet.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom"; // import Route and Routes
 import "./Pet.css";
 
 const iconMap = {
@@ -9,11 +10,15 @@ const iconMap = {
 export const Pet = ({ kind, pet }) => {
   const icon = iconMap[kind];
   return (
-    <article key={pet.id}>
-      <h3>
-        {icon} {pet.name}
-      </h3>
-    </article>
+    <Routes>
+      {/* ternary for routes if kind = cat then path is pets/cats if kind = dog then path is pets/dogs  */}
+      <Route path="/pets" element={
+        <article key={pet.id}>
+          <h3>
+            {icon} {pet.name}
+          </h3>
+        </article>} />
+      </Routes>
   );
 };
 
