9360ef7b1ff8f9f3ddc53486fe049875ca2c2973
juligarc91 <julissagarcia@pursuit.org>
2024-01-02 20:59:21 -0500

    imported BrowserRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // import BrowserRouter - to track URL addresses
 import App from "./App.jsx";
 import "./index.css";
 
