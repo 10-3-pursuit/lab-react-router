0eae782be1015bb3ed7935b5fef7062b9d9d28d3
Jose Barrios <josebarrios@pursuit.org>
2024-01-15 17:34:54 -0500

    Fixed indentation

--- src/components/pets/PetsList.jsx
+++ src/components/pets/PetsList.jsx
@@ -20,12 +20,11 @@ export const PetsList = ({ pets }) => {
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
       {kind === "cats" &&
-          cats.map((cat) => <Pet key={cat.id} kind="cat" pet={cat} />)}
-        {kind === "dogs" &&
-          dogs.map((dog) => <Pet key={dog.id} kind="dog" pet={dog} />)}
-          {kind !== "dogs" && kind !== "cats" &&
-          pets.map((pet) => <Pet key={pet.id} kind={pet.kind.toLowerCase()} pet={pet} />)}
-
+        cats.map((cat) => <Pet key={cat.id} kind="cat" pet={cat} />)}
+      {kind === "dogs" &&
+        dogs.map((dog) => <Pet key={dog.id} kind="dog" pet={dog} />)}
+      {kind !== "dogs" && kind !== "cats" &&
+        pets.map((pet) => <Pet key={pet.id} kind={pet.kind.toLowerCase()} pet={pet} />)}
       </section>
     </section>
   );
